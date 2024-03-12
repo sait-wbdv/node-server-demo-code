a2b2f0171988938845bb5ec425d9cd9e162a7862
Ashlyn Knox <ashlyn@lilyx.io>
2024-03-12 11:10:16 -0600

    simple html server

--- server.js
+++ server.js
@@ -12,10 +12,20 @@ const serverMessageListener = (req, res) => {
   res.end("This is better server code running!");
 };
 
-// DONE: Create a server
-const server = http.createServer(serverMessageListener);
+const pageData = "Hello world from a variable";
+const htmlMessageListener = (req, res) => {
+  // set the header
+  res.setHeader("Content-Type", "text/html");
+  // write the status code to the head
+  res.writeHead(200);
+  // send it some html
+  res.end(`<html><h1>${pageData}</h1></html>`);
+};
+// const server = http.createServer(serverMessageListener);
 
-// DONE: Set the server to listen
+// DONE: Create a server that serves html
+const server = http.createServer(htmlMessageListener);
+
 server.listen(port, host, () => {
   console.log(`server is running on http://${host}:${port} `);
 });
